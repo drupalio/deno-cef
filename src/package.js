ef23055714a32f290f9d7f5825a843413b2699ea
denjucks <denotutorials@protonmail.com>
2020-06-12 22:18:19 -0400

    Updated Electric to fix a big with packaging on windows and the method for starting the webview

--- src/package.js
+++ src/package.js
@@ -3,7 +3,11 @@ import * as path from "https://deno.land/std@0.55.0/path/mod.ts";
 import { copySync } from "https://deno.land/std@0.55.0/fs/mod.ts";
 
 export function cacheDenoModulesLocally() {
-	const denoCacheDir = env("deno", {suffix: ""}).cache;
+	let denoCacheDir = env("deno", {suffix: ""}).cache;
+	if (Deno.build.os === "windows") {
+		denoCacheDir = denoCacheDir.substr(0, denoCacheDir.length - 6)
+	}
+	
 	const localDenoModuleDir = path.join(Deno.cwd(), "deno_modules");
 	copySync(denoCacheDir, localDenoModuleDir);
 }
@@ -13,10 +17,13 @@ export function updateRunCommand() {
 		case "windows":
 			Deno.writeFileSync(path.join(Deno.cwd(), "run.vbs"), new TextEncoder().encode(`
 Set WshShell = CreateObject("WScript.Shell")
-Set ShellEnv = WshShell.Environment("SYSTEM") 
-ShellEnv("DENO_DIR") = "./deno_modules"
-WshShell.Run "deno.exe run -A app.js", 0
+WshShell.Run "env.bat", 0
 Set WshShell = Nothing
+`.trim()));
+
+Deno.writeFileSync(path.join(Deno.cwd(), "env.bat"), new TextEncoder().encode(`
+set DENO_DIR=./deno_modules
+deno.exe run -A app.js
 `.trim()));
 			break;
 
